b9e0680810f804639026736a15475b77f65efa28
SergioArias1 <sergiocreamundos@gmail.com>
2024-09-09 18:52:31 +0200

    hola he hecho commit

---
+++ src/User.java
@@ -0,0 +1,29 @@
+public class User {
+
+    private String nombre;
+
+    private String telefono;
+
+    private String email;
+
+    private int ID;
+
+    public User (String nombre, String telefono, String email){
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getTelefono(){
+        return this.telefono;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+}
